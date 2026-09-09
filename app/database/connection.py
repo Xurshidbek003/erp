60d2953a4e10c50d23570829036095a50c63d71d
Xurshidbek003 <xurshidbekt571@gmail.com>
2026-09-09 18:29:00 +0500

    first commit

---
+++ app/database/connection.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, \
+    AsyncSession
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+engine = create_async_engine(f"mysql+aiomysql://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}")
+
+
+SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession)
+
+
+async def get_db():
+    async with SessionLocal() as db:
+        yield db
